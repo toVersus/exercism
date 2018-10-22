dd19cb8bef094d8f4cc5e1552552f14968593375
toversus <toversus2357@gmail.com>
2018-10-22 20:30:00 +0900

    add scrabble-score-go

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,27 @@
+package scrabble
+
+import (
+	"strings"
+)
+
+var letterScores = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1,
+	'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5,
+	'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
+}
+
+// Score computes the scrabble score for given word.
+func Score(input string) int {
+	var total int
+	for _, l := range strings.ToUpper(input) {
+		if score, ok := letterScores[l]; ok {
+			total += score
+		}
+	}
+	return total
+}
